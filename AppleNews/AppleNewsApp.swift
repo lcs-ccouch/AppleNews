a928423b1f3e6d7d5dd510f7d70d9c77802c92b7
Calum Couch <ccouch@lcs.on.ca>
2023-05-23 11:34:50 -0400

    Initial Commit

---
+++ AppleNews/AppleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsApp.swift
+//  AppleNews
+//
+//  Created by calum couch on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
