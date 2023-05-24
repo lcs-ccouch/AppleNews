6a11b07d117604b715bfb7d55e5461c07dcf6203
Calum Couch <ccouch@lcs.on.ca>
2023-05-23 20:53:50 -0400

    ui is there just need to add borders so it all fits

--- AppleNews/Views/NewsListView.swift
+++ AppleNews/Views/NewsListView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 struct NewsListView: View {
     var body: some View {
         ScrollView {
-            NewsItemView(image: "Farming", headline: "Maritime farmers holding breath as recod-dry spring wrings region", TimeSincePosted: "1h ago", source:"GlobeAndMail")
-            
-             NewsItemView(image: "IcePatrol", headline: "After 100 years, the international Ice Patrol is winding down N.L. iceberg flights", TimeSincePosted: "1h ago", source:"CBCNEWS")
-                
-            NewsItemView(image: "E.JeanCarroll", headline: "E. Jean Carroll seeks $10 million in damages from Trump over post verdict statements", TimeSincePosted: "1h ago", source:"Reuters")
-            NewsItemView(image: "Sheep", headline: "New Zealand's ratio of sheep to humans at lowest point in 170 years", TimeSincePosted: "1h ago", source:"TheGuardian")
-           
+                HStack {
+                    NewsItemView(image: "Farming", headline: "Maritime farmers holding breath as recod-dry spring wrings region", TimeSincePosted: "1h ago", source:"GlobeAndMail")
+                    
+                    NewsItemView(image: "IcePatrol", headline: "After 100 years, the international Ice Patrol is winding down N.L. iceberg flights", TimeSincePosted: "1h ago", source:"CBCNEWS")
+                }
+            VStack {
+                NewsItemView(image: "E.JeanCarroll", headline: "E. Jean Carroll seeks $10 million in damages from Trump over post verdict statements", TimeSincePosted: "1h ago", source:"Reuters")
+                NewsItemView(image: "Sheep", headline: "New Zealand's ratio of sheep to humans at lowest point in 170 years", TimeSincePosted: "1h ago", source:"TheGuardian")
+            }
             
         }
     }
